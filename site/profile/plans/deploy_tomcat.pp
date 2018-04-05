bac1f2429e996f55fbb17c8da7cd5f36fdde179f
ipcrm <mcadorette@gmail.com>
2018-04-05 07:16:53 -0400

    init plan

---
+++ site/profile/plans/deploy_tomcat.pp
@@ -0,0 +1,22 @@
+plan profile::deploy_tomcat (
+  TargetSpec $app_servers,
+  String $deploy_location,
+  String $artifactory_base,
+  String $repository,
+  String $token,
+){
+
+  # name, version, group
+  $details = profile::loaddata('app.yaml')
+
+  run_task('profile:get_gavc', $app_servers,
+    artifactid=$details['name'],
+    version=$details['version'],
+    group=$details['group'],
+    repository=$repository,
+    deploy_location=$deploy_location,
+    artifactory_host=$artifactory_base,
+    apitoken=$token
+  )
+
+}
