2177dc5a7694c15e4dd1707c13648f72fdcbc8fb
Dimitri Tischenko <dimitri@puppet.com>
2018-06-11 19:23:57 +0200

    added hosts profile for initializing the IP

---
+++ site/profile/manifests/app/cd4pe_buildserver/hosts.pp
@@ -0,0 +1,17 @@
+#
+class profile::app::cd4pe_buildserver::hosts {
+
+  $master_server = $::settings::server
+  
+  $cd4pe_query = "facts[value]{ name in ['ipaddress_enp0s8',  'ipaddress_eth0']
+    and certname in resources[certname] { type = 'Class' and title = 'Role::Cd4pe' } }"
+
+  $cd4pe_ip = puppetdb_query($master_query)[0]['value']
+
+  host { 'cd4pe.pdx.puppet.vm':
+    ensure       => 'present',
+    ip           => $cd4pe_ip,
+    host_aliases => 'cd4pe',
+  }
+
+}
